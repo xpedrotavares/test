e316a894766055950b710cc5d6cc9cb2d76e6a65
xpedrotavares <xpedrotavaresdev@gmail.com>
2020-10-29 19:19:19 -0300

    basic comment form done

---
+++ client/src/App.js
@@ -0,0 +1,17 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>Comentário:</h1>
+    <div className="form">
+      <label> Comentário</label>
+      <textarea type="text" name="comment"/>
+      <button>Cadastrar</button>
+<div>teste</div>
+    </div>  
+    </div>
+  );
+}
+
+export default App;
