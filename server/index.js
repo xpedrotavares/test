a2d2f7b880cae67a927d8e1589b11d6a89aa4411
Pedro Tavares <xpedrotavaresdev@gmail.com>
2020-10-30 00:46:26 -0300

    connection server with db done

--- server/index.js
+++ server/index.js
@@ -1,11 +1,22 @@
-const express = require('express')
+const express = require("express")
 const app = express()
+const mysql = require("mysql")
 
+const db = mysql.createPool({
+    host: "localhost",
+    user: "root",
+    password: "",
+    database: "smarkioDatabase",
+});
 
-app.get('/', (req, res) => {
-    res.send("hello world")
+app.get("/", (req, res) => {
+    const sqlInsert = "INSERT INTO comments (comment, name) VALUES ('good', 'pedro alm');"
+    db.query(sqlInsert, (err, result) =>{
+        res.send("hello world")
+
+    })
 });
 
 app.listen(4000, () => {
-    console.log('running on port 4000')
+    console.log("running on port 4000")
 });
